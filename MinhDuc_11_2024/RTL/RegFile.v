5dbe55f73b058287bf1f0838036c750f7ff5f9c2
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-22 20:49:52 +0700

    Update RegFile.v

--- MinhDuc_11_2024/RTL/RegFile.v
+++ MinhDuc_11_2024/RTL/RegFile.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/13/2024 09:52:43 PM
-// Design Name: 
-// Module Name: RegFlile
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module RegFile(
