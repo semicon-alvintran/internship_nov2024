52a3d846944a68dfa80987517f1deb2f01d907bf
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-22 20:50:28 +0700

    Update mux2to1.v

--- MinhDuc_11_2024/RTL/mux2to1.v
+++ MinhDuc_11_2024/RTL/mux2to1.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/15/2024 06:09:56 PM
-// Design Name: 
-// Module Name: mux2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module mux2to1(
