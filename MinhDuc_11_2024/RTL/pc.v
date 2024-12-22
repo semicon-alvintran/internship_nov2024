88cd73a36508c0b5f5ea4f2313d23cb6770b6d7b
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-22 20:50:43 +0700

    Update pc.v

--- MinhDuc_11_2024/RTL/pc.v
+++ MinhDuc_11_2024/RTL/pc.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/13/2024 10:31:47 PM
-// Design Name: 
-// Module Name: pc
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module pc(
