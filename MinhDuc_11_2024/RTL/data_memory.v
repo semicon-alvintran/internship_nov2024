16c181625cafdf9bceda72742c4b59bbcff8a717
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-22 20:50:17 +0700

    Update data_memory.v

--- MinhDuc_11_2024/RTL/data_memory.v
+++ MinhDuc_11_2024/RTL/data_memory.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/13/2024 10:34:46 PM
-// Design Name: 
-// Module Name: data_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module data_memory(
