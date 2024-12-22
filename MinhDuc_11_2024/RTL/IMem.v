a82bfa3148735a13abb35cee786f55aa0550a98e
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-22 20:49:41 +0700

    Update IMem.v

--- MinhDuc_11_2024/RTL/IMem.v
+++ MinhDuc_11_2024/RTL/IMem.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/13/2024 10:36:28 PM
-// Design Name: 
-// Module Name: IMem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module IMem(
