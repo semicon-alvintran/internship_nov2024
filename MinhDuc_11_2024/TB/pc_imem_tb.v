25067981d32f5e6b3e4f9930354fe511190791bc
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-23 17:46:20 +0700

    Update pc_imem_tb.v

--- MinhDuc_11_2024/TB/pc_imem_tb.v
+++ MinhDuc_11_2024/TB/pc_imem_tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/19/2024 02:16:05 PM
-// Design Name: 
-// Module Name: pc_imem_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module pc_imem_tb;
