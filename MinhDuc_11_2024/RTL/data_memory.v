96766f56321f58076cddbee317f4c560b642f682
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-23 17:44:12 +0700

    Update data_memory.v

--- MinhDuc_11_2024/RTL/data_memory.v
+++ MinhDuc_11_2024/RTL/data_memory.v
@@ -5,16 +5,24 @@
 module data_memory(
     input clk, reset, MemWrite, MemRead,
     input [31:0] Address, writeData,
-    output [31:0] readData
+    output reg [31:0] readData
     );
      
     reg [31:0] Dmemory [63:0];  
     wire [5:0] memAddress = Address[7:2];  
-    
-    assign readData = (MemRead) ? Dmemory[memAddress] : 32'h0;
     integer i;
-    always @ (posedge clk) begin
-        if(reset == 1'b1) begin
+    //assign readData = (MemRead) ? Dmemory[memAddress] : 32'h0;
+    //integer i;
+    always @(*) begin   
+        if(MemRead) begin   
+            readData = Dmemory[memAddress];
+        end 
+        else begin
+            readData = 32'h0;
+        end
+    end
+    always @ (posedge clk or posedge reset) begin
+        if(reset) begin
             for(i = 0; i < 64; i = i + 1) begin
                 Dmemory[i] <= 32'h0;  
             end
@@ -24,3 +32,4 @@ module data_memory(
         end
     end
 endmodule
+
