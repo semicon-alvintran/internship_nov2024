457aa92dcb36c6a0b4ecbf0256270ffd79c7ab2e
Phan Minh Đức <113233216+phanminhduc241202@users.noreply.github.com>
2024-12-24 10:57:42 +0700

    Update immGen_tb.v

--- MinhDuc_11_2024/TB/immGen_tb.v
+++ MinhDuc_11_2024/TB/immGen_tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/17/2024 11:45:58 PM
-// Design Name: 
-// Module Name: immGen_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module immGen_tb;
